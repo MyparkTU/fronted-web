1a140df7fb87bf6ad4c7bd432d3bc74da369b566
thanks <phuwich.winyutrakul@gmail.com>
2022-11-06 02:28:36 +0700

    Update ParkTable Component
    ParkTable V1

---
+++ src/components/Test.js
@@ -0,0 +1,31 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
+
+function NavTabsExample() {
+  return (
+    <Card>
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+          <Nav variant="pills" className="flex-column">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Tab.Content>
+            <Tab.Pane eventKey="first">
+              <p>hi</p>
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <p>hello</p>
+            </Tab.Pane>
+          </Tab.Content>
+    </Tab.Container>
+    </Card>
+  );
+}
+
+export default NavTabsExample;
