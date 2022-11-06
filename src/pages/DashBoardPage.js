4823e50ae8adfb9fdadac84f2bd4c8fde53dc856
arcsad <94112824+arcsad@users.noreply.github.com>
2022-11-06 18:20:10 +0700

    update

--- src/pages/DashBoardPage.js
+++ src/pages/DashBoardPage.js
@@ -1,9 +1,10 @@
 import SideBar from "../components/SideBar";
 import Headers from "../components/header";
-
+import '../styles/DashBoard.css'
 function DashboardPage() {
     return(
         <div> 
+            <h2 className="headerlabel">สถานที่จอดรถทั้งหมด</h2>
             <Headers />
             <SideBar />
         </div>
